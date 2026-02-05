12d0b2fe624e1e6073da638755eb0d9c7a197c00
Naruto Yamada <is0748vi@ed.ritsumei.ac.jp>
2026-02-05 11:37:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("button").addEventListener("click", function(){
+    const name = document.getElementById("name").value.trim();
+    const jname = document.getElementById("jname").value.
+})
